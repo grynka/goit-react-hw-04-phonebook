8fb0c187daf74679f30b1f1024cbd080685d7f79
grynka <ihor.kozhemyakin@gmail.com>
2022-12-15 13:42:34 +0200

    Update ContactForm.js

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,10 +1,65 @@
-import React, { Component } from "react"
+import React, { Component, useState } from "react"
 import { nanoid } from "nanoid";
 import { Label, Input, Forms, Button } from './ContactForm.styled';
 import PropTypes from 'prop-types';
 
- 
-class ContactForm extends Component {
+export default  function ContactForm() {
+const = [name, setName] = useState('');
+const = [number, setNumber] = useState('');
+
+handleChange = event => {
+  const { value } = event.currentTarget;
+  setName: value;
+};
+
+handleContactsCreate = event => {
+    event.preventDefault();
+    this.props.onSubmit({
+      name: this.state.name,
+      number: this.state.number,
+      id: nanoid(),
+    });
+  this.setState({
+    name: "",
+    number: ""
+  }
+
+return (
+  <Forms onSubmit={handleContactsCreate}>
+    <Label>
+      Name
+      <Input
+        value={name}
+        type="text"
+        name="name"
+        onChange={handleChange}
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+    </Label>
+    <Label>
+      Phone
+      <Input
+        type="tel"
+        name="number"
+        value={number}
+        onChange={handleChange}
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      />
+    </Label>
+    <p>
+      <Button type="submit">Add contact</Button>
+    </p>
+  </Forms>
+);
+}
+
+
+
+class ContactFormold extends Component {
     state = {
       name: '',
       number: '',
@@ -67,7 +122,7 @@ class ContactForm extends Component {
     }
   }
 
-export default ContactForm
+export {ContactFormold}
     
 ContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
